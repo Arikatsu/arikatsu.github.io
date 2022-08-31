6a1e7f882a0cf331e331c2f06986d8433f6bcd37
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-09-01 01:23:20 +0530

    redirect change and menu popups

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -4,7 +4,7 @@ const __pfp = document.getElementsByClassName("pfp-container")[0];
 __pfp.addEventListener("click", () => {
     __clicks++;
     if (__clicks === 3) {
-        window.location.replace("https://images.arikatsu.lol/");
+        window.open("https://images.arikatsu.lol/");
         __clicks = 0;
     }
 });
